b4feb129a532708e09cbcb7378663e0dd1223374
oli <oliverk5578@gmail.com>
2026-05-14 19:13:02 -0400

    fix: add legacy plugin for iOS Safari support

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({ targets: ['ios >= 13', 'safari >= 13'] }),
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:3001'
