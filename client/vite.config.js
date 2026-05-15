682cf5f984adc61bd6381a29b3c1ecbe99a02e30
oli <oliverk5578@gmail.com>
2026-05-14 21:36:16 -0400

    fix: remove color-scheme dark + dvh, target safari13 for black screen fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: ['es2020', 'safari14'],
+    target: ['es2019', 'safari13'],
     rollupOptions: {
       output: {
         manualChunks: {
